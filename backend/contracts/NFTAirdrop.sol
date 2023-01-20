b791118c01df0b4c001ffb424a4800ba94d158e0
jameel9314 <sondusjameel9314@gmail.com>
2023-01-20 20:29:41 +0500

    add constructor and verify function to contract.

--- backend/contracts/NFTAirdrop.sol
+++ backend/contracts/NFTAirdrop.sol
@@ -1,3 +1,22 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract NFTAirdrop {
+    bytes32 private immutable root;
+
+    constructor(bytes32 _root) {
+        root = _root;
+    }
+
+    function verify(bytes32[] memory _proof) public view returns (bool) {
+        return
+            MerkleProof.verify(
+                _proof,
+                root,
+                keccak256(abi.encodePacked(msg.sender))
+            );
+    }
+}
