63d08ffbb7841cb02de9bca52b5f2e3bcb494135
lumi518 <aurora518star.com@gmail.com>
2023-01-23 19:58:59 +0500

    setup beforeEach unit tet

--- backend/test/unit/nft-drop.test.ts
+++ backend/test/unit/nft-drop.test.ts
@@ -1,7 +1,26 @@
-import { network } from "hardhat";
+import { deployments, ethers, getNamedAccounts, network } from "hardhat";
 import { developmentChains } from "../../helper-hardhat-config";
+import { NFTAirdrop } from "../../typechain-types";
+import { generateMerkleTree } from "../../scripts/generate-merkle-tree";
+import MerkleTree from "merkletreejs";
 
 // * if the newwork will be hardhat or localhost then these tests will be run.
 !developmentChains.includes(network.name)
     ? describe.skip
-    : describe("NFTAirdrop Unit Tests", () => {});
+    : describe("NFTAirdrop Unit Tests", () => {
+          let deployer: string;
+          let nftAirdrop: NFTAirdrop;
+          let tree: MerkleTree;
+
+          beforeEach(async () => {
+              if (!developmentChains.includes(network.name)) {
+                  throw "You need to be on a development chain to run tests";
+              }
+              deployer = (await getNamedAccounts()).deployer;
+              await deployments.fixture(["all"]);
+
+              nftAirdrop = await ethers.getContract("NFTAirdrop", deployer);
+
+              tree = await generateMerkleTree();
+          });
+      });
