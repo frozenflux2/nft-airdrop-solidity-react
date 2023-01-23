2dc70a74228659de8d3fad2b9df31e9cd57c4886
lumi518 <aurora518star.com@gmail.com>
2023-01-23 15:41:12 +0500

    import ERC721 and Ownable contracts and initialize the constructor.

--- backend/contracts/NFTAirdrop.sol
+++ backend/contracts/NFTAirdrop.sol
@@ -3,12 +3,26 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTAirdrop {
+contract NFTAirdrop is ERC721URIStorage, Ownable {
     bytes32 private immutable root;
+    string private nftTokenUri;
+    uint256 private immutable mintFee;
+    uint256 private tokenCounter;
 
-    constructor(bytes32 _root) {
+    constructor(
+        bytes32 _root,
+        string memory _name,
+        string memory _symbol,
+        string memory _nftTokenUri,
+        uint256 _mintFee
+    ) ERC721(_name, _symbol) {
         root = _root;
+        nftTokenUri = _nftTokenUri;
+        mintFee = _mintFee;
+        tokenCounter = 0;
     }
 
     function verify(bytes32[] memory _proof) public view returns (bool) {
