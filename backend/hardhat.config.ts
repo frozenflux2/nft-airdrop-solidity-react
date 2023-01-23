932d1a8602ff81f76d4868fe3823e08543dcf058
jameel9314 <sondusjameel9314@gmail.com>
2023-01-23 16:10:22 +0500

    add hardhat-deploy plugin

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
 };
 
 export default config;
