2a85b4ac4c1c35cc1bbd5028bc09e7e5ddeb2263
lumi518 <aurora518star.com@gmail.com>
2023-01-23 18:04:59 +0500

    add configurations to hardhat.config.ts file

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -5,6 +5,29 @@ import "dotenv/config";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            chainId: 5,
+            url: process.env.GOERLI_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY!],
+            saveDeployments: true,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
