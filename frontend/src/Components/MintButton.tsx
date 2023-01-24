f53e26e3df8955500b74017eae1f87bfe827097e
jameel9314 <sondusjameel9314@gmail.com>
2023-01-24 18:45:00 +0500

    Extract MintButton and SwitchNetworkButton components

---
+++ frontend/src/Components/MintButton.tsx
@@ -0,0 +1,12 @@
+function MintButton() {
+    return (
+        <button
+            onClick={() => {}}
+            className="bg-gradient-to-r from-[#60c657] to-[#35aee2] px-24 py-4 text-xl text-white font-bold rounded-lg"
+        >
+            Mint
+        </button>
+    );
+}
+
+export default MintButton;
