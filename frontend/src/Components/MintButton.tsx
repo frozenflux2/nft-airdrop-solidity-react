f9228ddd329e5a4736d76ec1c86bb22ba8e94c15
lumi518 <aurora518star.com@gmail.com>
2023-01-25 15:36:14 +0500

    mint nft and show success and error notifications respectivily.

--- frontend/src/Components/MintButton.tsx
+++ frontend/src/Components/MintButton.tsx
@@ -1,22 +1,116 @@
 import { useEffect, useState } from "react";
-import { generateMerkleTree } from "../utils/generate-merkle-tree";
 import MerkleTree from "merkletreejs";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import { abi } from "../constants";
+import { NFT } from "../App";
+import { useNotification } from "@web3uikit/core";
+import { ContractTransaction } from "ethers";
+import { AiFillBell } from "react-icons/ai";
+import { keccak256 } from "ethers/lib/utils";
 
-function MintButton() {
-    const [tree, setTree] = useState<MerkleTree | null>(null);
+interface Props {
+    nftContractAddress: string;
+    nftIndex: NFT | null;
+    treeInstance: MerkleTree | null;
+}
+
+function MintButton(props: Props) {
+    const dispatch = useNotification();
+    const { account } = useMoralis();
+
+    const [proof, setProof] = useState<string[] | undefined>();
 
     useEffect(() => {
         (async () => {
-            const treeInstance = await generateMerkleTree();
-            setTree(treeInstance);
-            // alert("check");
+            generateProof();
         })();
-    }, []);
+    }, [account]);
 
+    const { runContractFunction: preMintNFT } = useWeb3Contract({
+        abi: abi,
+        contractAddress: props.nftContractAddress,
+        functionName: "preMintNFT",
+        params: {
+            _proof: proof,
+            _tokenUriIndex: props.nftIndex,
+        },
+    });
+
+    async function generateProof() {
+        if (account != null) {
+            const hashAddress = keccak256(account?.toString());
+            const proof = props.treeInstance?.getHexProof(hashAddress);
+
+            if (proof != undefined) {
+                setProof(proof);
+            }
+        }
+    }
+
+    async function handleOnPreMint() {
+        if (props.nftIndex == null) {
+            dispatch({
+                type: "warning",
+                title: "NFT Selection Error",
+                message:
+                    "No NFT has been selected for this transaction. Please make sure to select an NFT before proceeding with the transaction.",
+                icon: <AiFillBell />,
+                position: "topR",
+            });
+            return;
+        }
+
+        await preMintNFT({
+            onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
+            onError: (error) => {
+                handleErrors(error);
+            },
+        });
+    }
+
+    async function handleSuccess(tx: ContractTransaction): Promise<void> {
+        await tx.wait(1);
+        dispatch({
+            type: "success",
+            title: "NFT Minting Successful",
+            message:
+                " Your NFT has been minted successfully and is now available for use.",
+            icon: <AiFillBell />,
+            position: "topR",
+        });
+    }
+
+    function handleErrors(error: Error) {
+        if (error.message.includes("User denied transaction signature.")) {
+            showErrorNotification(
+                "Permission Denied",
+                "User denied transaction signature."
+            );
+        } else if (error.message.toLowerCase().includes("nonce too high")) {
+            showErrorNotification("Invalid Nonce", "Reset your Metamask.");
+        } else if (error.message.includes("Internal JSON-RPC error.")) {
+            showErrorNotification(
+                "Transaction Failed",
+                "We apologize for the inconvenience. Please check the details of your transaction and try again."
+            );
+        } else {
+            showErrorNotification(error.name, error.message);
+        }
+    }
+
+    function showErrorNotification(title: string, message: string) {
+        dispatch({
+            type: "error",
+            title: title,
+            message: message,
+            icon: <AiFillBell />,
+            position: "topR",
+        });
+    }
 
     return (
         <button
-            onClick={() => {}}
+            onClick={handleOnPreMint}
             className="bg-gradient-to-r from-[#60c657] to-[#35aee2] px-24 py-4 text-xl text-white font-bold rounded-lg"
         >
             Mint
--- frontend/src/Components/NFTSRow.tsx
+++ frontend/src/Components/NFTSRow.tsx
@@ -6,67 +6,62 @@ import ChromaBot from "../assets/images/Chroma_Bot.png";
 import SunnyPaws from "../assets/images/Sunny_Paws.png";
 import RockingWhiskers from "../assets/images/Rocking_Whiskers.png";
 import MuscleMoo from "../assets/images/Muscle_Moo.png";
+import { NFT } from "../App";
 
-enum NFT {
-    Nico,
-    AzuraBlaze,
-    ChromaBot,
-    SunnyPaws,
-    RockingWhiskers,
-    MuscleMoo,
+interface Props {
+    nftIndex: NFT | null;
+    setNFTIndex: React.Dispatch<React.SetStateAction<NFT | null>>;
 }
 
-function NFTSRow() {
-    const [nft, setNFT] = useState<NFT | null>(null);
-
+function NFTSRow(props: Props) {
     return (
         <div className="flex mx-16 mt-8">
             <ImageCard
                 title="Nico"
                 imageSource={Nico}
-                isSelected={nft == NFT.Nico}
+                isSelected={props.nftIndex == NFT.Nico}
                 onClick={() => {
-                    setNFT(NFT.Nico);
+                    props.setNFTIndex(NFT.Nico);
                 }}
             />
             <ImageCard
                 title="Azura Blaze"
                 imageSource={AzuraBlaze}
-                isSelected={nft == NFT.AzuraBlaze}
+                isSelected={props.nftIndex == NFT.AzuraBlaze}
                 onClick={() => {
-                    setNFT(NFT.AzuraBlaze);
+                    props.setNFTIndex(NFT.AzuraBlaze);
                 }}
             />
             <ImageCard
                 title="Chroma Bot"
                 imageSource={ChromaBot}
-                isSelected={nft == NFT.ChromaBot}
+                isSelected={props.nftIndex == NFT.ChromaBot}
                 onClick={() => {
-                    setNFT(NFT.ChromaBot);
+                    props.setNFTIndex(NFT.ChromaBot);
                 }}
             />
             <ImageCard
                 title="Sunny Paws"
                 imageSource={SunnyPaws}
-                isSelected={nft == NFT.SunnyPaws}
+                isSelected={props.nftIndex == NFT.SunnyPaws}
                 onClick={() => {
-                    setNFT(NFT.SunnyPaws);
+                    props.setNFTIndex(NFT.SunnyPaws);
                 }}
             />
             <ImageCard
                 title="Rocking Whiskers"
                 imageSource={RockingWhiskers}
-                isSelected={nft == NFT.RockingWhiskers}
+                isSelected={props.nftIndex == NFT.RockingWhiskers}
                 onClick={() => {
-                    setNFT(NFT.RockingWhiskers);
+                    props.setNFTIndex(NFT.RockingWhiskers);
                 }}
             />
             <ImageCard
                 title="Muscle Moo"
                 imageSource={MuscleMoo}
-                isSelected={nft == NFT.MuscleMoo}
+                isSelected={props.nftIndex == NFT.MuscleMoo}
                 onClick={() => {
-                    setNFT(NFT.MuscleMoo);
+                    props.setNFTIndex(NFT.MuscleMoo);
                 }}
             />
         </div>
